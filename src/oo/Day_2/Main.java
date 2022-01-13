51116cdb5ebd71edd9f34900d5fc7193b5299492
vietduc2608 <vieduc2608@gmail.com>
2022-01-13 16:53:21 +0700

    Nộp Bài Sinh Viên

--- src/oo/Day_2/Main.java
+++ src/oo/Day_2/Main.java
@@ -3,6 +3,8 @@ package oo.Day_2;
 public class Main {
     public static void main(String[] args) {
         SinhVienGDU sinhVienGDU = new SinhVienGDU(1, "Lê Việt Đức", "Nam", 20, "CNTT", 7.0);
+        SinhVienGDU sinhVienGDU = new SinhVienGDU(2, "Nguyễn Đình Khiêm", "Nam", 20, "CNTT", 6.0);
+        SinhVienGDU sinhVienGDU = new SinhVienGDU(3, "Vũ Hoàng Tuấn Anh", "Nam", 20, "CNTT", 4.0);
         sinhVienGDU.show();
     }
 }
