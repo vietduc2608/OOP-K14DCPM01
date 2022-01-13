b73c19dc33b6e199143ac812c4275520dddab43b
vietduc2608 <vieduc2608@gmail.com>
2022-01-13 16:07:53 +0700

    Nộp bài

---
+++ src/oo/Day_1/Customer.java
@@ -0,0 +1,27 @@
+package oo.Day_1;
+
+import java.util.Scanner;
+
+public class Customer {
+    int customer_id;
+    String name, address;
+
+    Scanner input = new Scanner(System.in); 
+
+
+    void Nhap(){
+        System.out.println("Nhap id Khach Hang");
+        customer_id = input.nextInt();
+        System.out.println("Nhap Ten Khach Hang: ");
+        name = input.nextLine();
+        System.out.println("Nhap dia chi: ");
+        address = input.nextLine();
+    }
+
+    void show() {
+        System.out.println("thong tin tai khoan");
+        System.out.println("Account id: " + customer_id);
+        System.out.println("Account name: " +name);
+        System.out.println("Account address: "+address);
+    }
+}
---
+++ src/oo/Day_2/SinhVienGDU.java
@@ -0,0 +1,49 @@
+package oo.Day_2;
+
+public class SinhVienGDU {
+    private int maSV;
+    private String hoTen;
+    private String diaChi;
+ 
+    public SinhVienGDU() {}
+    
+    public SinhVienGDU(int maSV, String hoTen, String diaChi) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diaChi = diaChi;
+    }
+
+    //--------------begin getter and setter--------------------
+    public int getMaSV() {
+        return maSV;
+    }
+ 
+    public void setMaSV(int maSV) {
+        this.maSV = maSV;
+    }
+ 
+    public String getHoten() {
+        return hoTen;
+    }
+ 
+    public void setHoten(String hoten) {
+        this.hoTen = hoten;
+    }
+ 
+    public String getDiaChi() {
+        return diaChi;
+    }
+ 
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+    //--------------end getter and setter--------------------
+
+
+    public void show(){
+        System.out.println("======Thông Tin Sinh Viên======");
+        System.out.println("Mã Sinh Viên: "+maSV  +"Họ Tên: "+hoTen +"Địa chỉ: "+diaChi);
+        System.out.printf("%-5d %-20s %-15s\n", maSV, hoTen, diaChi);
+    }
+    
+}
